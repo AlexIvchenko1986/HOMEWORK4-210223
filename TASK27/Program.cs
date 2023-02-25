7c049879aba41dc8d544531ffb48151754568f05
Alexander Ivchenko <alex1986br@yandex.ru>
2023-02-25 18:27:16 +0300

    TASK27

---
+++ TASK27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+internal class Program
+{
+  static int sum(int abc)
+  {
+    int result = 0;
+    while (abc > 0)
+    {
+      result = result + abc % 10;
+      abc = abc / 10;
+    }
+    return result;
+  }
+  static void Main(string[] args)
+  {
+    int Sumabc = 9012;
+    int result = sum(Sumabc);
+    Console.WriteLine(result);
+  }
+}
