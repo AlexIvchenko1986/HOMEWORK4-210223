f589f73da9d17ddcdfb1c8c1c0939edba411864c
Alexander Ivchenko <alex1986br@yandex.ru>
2023-02-25 13:18:40 +0300

    TASK25

---
+++ TASK25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//Math.Pow использовать нельзя
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+
+int a = 3;
+int b = 5;
+int stepen = 1;
+void Stepen()
+{
+  for (int i = 1; i <= b; i++)
+  {
+    stepen = stepen * a;
+  }
+  Console.WriteLine(stepen);
+}
+Stepen();
